84b10581ff381ca207fc47b88ff91c86988739fc
Daniel Eckelt <daniel.eckelt8@gmail.com>
2014-09-23 21:31:29 +0200

    PaymentApp Reversioning
    Complete change of the app. now always centered and nicely structured

--- PaymentApp/controller/AppCtrl.js
+++ PaymentApp/controller/AppCtrl.js
@@ -1,5 +1,15 @@
 var module = angular.module("paymentApp");
 
-module.controller("AppCtrl", ["$scope", function($scope){
+module.controller("AppCtrl", ["$scope", function($scope) {
 	
+	// selection is valid at beginning, if preset!
+/*	$scope.sum = 0.0;
+	$scope.payer = 0.0;
+	$scope.afterPayer = 0.0;
+	$scope.solution = 0.0;
+*/
+	$scope.solution = function(){
+		return ($scope.sum - $scope.payer + $scope.afterPayer) / 2;
+	}
+
 }]);
