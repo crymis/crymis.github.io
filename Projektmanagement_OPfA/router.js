9b1919c99c62463dafd964cea5e328ed11d7f267
Daniel Eckelt <daniel.eckelt8@gmail.com>
2014-11-08 23:45:02 +0100

    router change

--- Projektmanagement_OPfA/router.js
+++ Projektmanagement_OPfA/router.js
@@ -2,14 +2,14 @@ var module = angular.module('portingGuide');
 module.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
 				when("/home", {
-					templateUrl: "/views/home.html",
+					templateUrl: "views/home.html",
 					controller: "HomeCtrl"
 				}).
 				when("/about", {
 					templateUrl: "views/about.html"
 				}).
 				when("/result", {
-					templateUrl: "/views/result.html",
+					templateUrl: "views/result.html",
 					controller: "HomeCtrl"
 				}).
 				otherwise({
