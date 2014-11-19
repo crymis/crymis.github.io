8b90424fe7dfcdf2590a9975f183ec4116bfa0b9
Daniel Eckelt <daniel.eckelt8@gmail.com>
2014-11-19 22:42:39 +0100

    Added bandGallery directive for demo
    Demo of the bandGallery directive made with AngularJS

---
+++ BandGallery/router.js
@@ -0,0 +1,11 @@
+var module = angular.module('bandGalleryDemo');
+module.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	when("/", {
+		templateUrl: "/views/home.html",
+		controller: "HomeCtrl"
+	}).
+	otherwise({
+		redirectTo: "/"
+	});
+}]);
