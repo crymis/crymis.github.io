0c1a98b6c586d21768d1fc3cb8d6c1ef857648c7
Daniel Eckelt <daniel.eckelt8@gmail.com>
2014-11-30 21:58:08 +0100

    specify top and down btns separatly
    now you can config more specific which button is visibly

--- BandGallery/directives/bandGallery.js
+++ BandGallery/directives/bandGallery.js
@@ -11,19 +11,21 @@ module.directive("bandGallery", ["$window", function($window){
 		restrict: 'E',
 		scope: {
 			"pageNr": "=",
-			"navBtns": "=",
+			"navTopBtn": "=",
+			"navDownBtn" : "=",
 			"bandGalleryImgs": "=galleryImgs"
 		},
 		link: function(scope, element, attrs) {
 
 			// checking for attributes: set true, if undefined
-			scope.btnsActive = scope.navBtns === undefined || !!scope.navBtns;
+			scope.topBtnActive = scope.navTopBtn === undefined || !!scope.navTopBtn;
+			scope.downBtnActive = scope.navDownBtn === undefined || !!scope.navDownBtn;
 			scope.pageNrActive = scope.pageNr === undefined || !!scope.pageNr;
 			
 			scope.prevImg = function(index) {
 				if(index-1 > -1) {
 					angular.element('html,body').animate({
-						scrollTop: angular.element('.band-'+(index-1)).offset().top+1
+						scrollTop: angular.element('.band-'+(index-1)).offset().top
 					}, 1000, "easeInOutCubic");
 				}
 			};
@@ -31,7 +33,7 @@ module.directive("bandGallery", ["$window", function($window){
 			scope.nextImg = function(index) {
 				if(index+1 < scope.bandGalleryImgs.length) {
 					angular.element('html,body').animate({
-						scrollTop: angular.element('.band-'+(index+1)).offset().top+1
+						scrollTop: angular.element('.band-'+(index+1)).offset().top
 					}, 1000, "easeInOutCubic");
 				}
 			};
@@ -55,7 +57,10 @@ module.directive("bandGallery", ["$window", function($window){
 			angular.element($window).bind('resize', function() {
 				// only if windows new size changes more than 10%, rescaling is done
 				var newVal = $window.innerHeight;
-				if(Math.abs(newVal - oldVal) > (newVal * 0.15)) {
+				if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && Math.abs(newVal - oldVal) > (newVal * 0.15)) {
+					adjustHeights();
+					scope.$apply();
+				} else {
 					adjustHeights();
 					scope.$apply();
 				}
@@ -64,9 +69,9 @@ module.directive("bandGallery", ["$window", function($window){
 		},
 		template: 
 		"<div data-ng-repeat='band in bandGalleryImgs' class='band band-{{$index}}' style='background-image: url({{band.url}}); height:{{windowHeight}}px'>" +
-			"<button class='overlayBtn prevBtn' data-ng-click='prevImg($index)' data-ng-show='btnsActive && !$first'> &gt; </button>" +
-			"<button class='overlayBtn nextBtn' data-ng-click='nextImg($index)' data-ng-show='btnsActive && !$last'> &gt; </button>" +
-			"<button class='overlayBtn goTopBtn' data-ng-click='goTop()' data-ng-show='btnsActive && $last'> &gt;&gt; </button>" +
+			"<button class='overlayBtn prevBtn' data-ng-click='prevImg($index)' data-ng-show='topBtnActive && !$first'> &gt; </button>" +
+			"<button class='overlayBtn nextBtn' data-ng-click='nextImg($index)' data-ng-show='downBtnActive && !$last'> &gt; </button>" +
+			"<button class='overlayBtn goTopBtn' data-ng-click='goTop()' data-ng-show='downBtnActive || topBtnActive && $last'> &gt;&gt; </button>" +
 			"<h1 class='band-heading'>{{band.title}}</h1>" +
 			"<p class='band-description'>{{band.description}}</p>" +
 			"<div class='pageNr' data-ng-show='pageNrActive'> {{$index+1}}/{{bandGalleryImgs.length}} </div>" +
