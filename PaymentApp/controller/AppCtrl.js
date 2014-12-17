47796d8dc34c4c4d729ef27b53bbc712722fd82e
Daniel Eckelt <daniel.eckelt@inovex.de>
2014-12-17 15:54:41 +0100

    Added to EPIC MODE button!

--- PaymentApp/controller/AppCtrl.js
+++ PaymentApp/controller/AppCtrl.js
@@ -8,8 +8,29 @@ module.controller("AppCtrl", ["$scope", function($scope) {
 	$scope.afterPayer = 0.0;
 	$scope.solution = 0.0;
 */
+	$scope.newStyle = "EPIC MODE";
+	$scope.epicMode = false;
+
 	$scope.solution = function(){
 		return ($scope.sum - $scope.payer + $scope.afterPayer) / 2;
-	}
+	};
+
+	$scope.switchStyle = function() {
+		$scope.epicMode = !$scope.epicMode;
+		if ($scope.newStyle === "EPIC MODE") {
+			$scope.newStyle = "NORMAL MODE";
+			for (var i = 0; i < document.getElementsByTagName("*").length; i++) {
+				document.getElementsByTagName("*")[i].style.color = "#F5EDB5";
+				document.getElementsByTagName("*")[i].style.borderColor = "#F5EDB5";
+			}
+		} else {
+			$scope.newStyle = "EPIC MODE";
+			for (var i = 0; i < document.getElementsByTagName("*").length; i++) {
+				document.getElementsByTagName("*")[i].style.color = "#59575a";
+				document.getElementsByTagName("*")[i].style.borderColor = "#59575a";
+			}
+		}
+		
+	};
 
 }]);
